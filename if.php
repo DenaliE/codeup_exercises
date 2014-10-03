895a8bf090bad3cbbf788168bc2a17cda99dca90
DenaliE <denali.endicott@grok-interactive.com>
2014-10-03 14:54:20 -0500

    If, else, and ifelse blocks added

--- if.php
+++ if.php
@@ -44,4 +44,112 @@
      echo "$b is not identical to '$c'\n";
  }
 
+//  / Shorten the next 2 statements into an if/else
+// if ($a < $b) {
+//     // output the appropriate result
+//     echo "$a is less than $b";
+// }
+
+// if ($b > $a) {
+//     // output the appropriate result
+//     echo "$b is greater than $a";
+
+// Shorten the next 2 statements into an if/else
+if ($a < $b) {
+    // output the appropriate result
+    echo "$a is less than $b\n";}
+else {
+     echo "$a is not less than $b\n";
+} 
+
+
+if ($b > $a) {
+    // output the appropriate result
+    echo "$b is greater than $a\n";
+}
+
+else {
+     echo "$b is not greater than $a\n";
+}
+
+// // Shorten the next 2 statements into an if/else
+// if ($b >= $c) {
+//     // output the appropriate result
+//     echo "$b is ____ $c\n";
+// }
+
+// if ($b <= $c) {
+//     // output the appropriate result
+//     echo "$b is ____ $c\n";
+// }
+
+// Shorten the next 2 statements into an if/else
+if ($b >= $c) {
+    // output the appropriate result
+    echo "$b is greater than or equal to $c\n";
+}
+
+else {
+     echo "$b is not greater than or equal to $c\n";
+}
+
+if ($b <= $c) {
+    // output the appropriate result
+    echo "$b is less than or equal to $c\n";
+}
+
+else {
+     echo "$b is not less than or equal to $c\n";
+}
+
+// // combine the next 4 conditionals into
+// // one if/else/elseif block that checks in order for:
+// // identical, equal, not identical, not equal
+// if ($b == $c) {
+//     // output the appropriate result
+//     echo "$b is equal to $c";
+// }
+
+// if ($b === $c) {
+//     // output the appropriate result
+//     echo "$b is identical to $c";
+// }
+
+// if ($b != $c) {
+//     // output the appropriate result
+//     echo "$b is not equal to $c";
+// }
+
+// if ($b !== $c) {
+//     // output the appropriate result
+//     echo "$b is not identical to $c";
+// }
+
+// combine the next 4 conditionals into
+// one if/else/elseif block that checks in order for:
+// identical, equal, not identical, not equal
+if ($b === $c) {
+    // output the appropriate result
+    echo "$b is identical to '$c'";
+}
+
+elseif ($b == $c) {
+    // output the appropriate result
+    echo "$b is equal to '$c'";
+}
+
+elseif ($b !== $c) {
+    // output the appropriate result
+    echo "$b is not identical to '$c'";
+}
+
+elseif ($b != $c) {
+    // output the appropriate result
+    echo "$b is not equal to '$c'";
+}
+
+else {
+     echo "None of these apply.";
+}
+
  ?>

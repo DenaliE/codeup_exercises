fef60391897b2c64c65f57ae0167be18accfad61
DenaliE <denali.endicott@grok-interactive.com>
2014-10-07 10:20:26 -0500

    Completed exercise

--- foreach.php
+++ foreach.php
@@ -2,6 +2,41 @@
 
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
-foreach ($things as $value) {
-    echo $value . "\n";
+//#2 echo values with data type
+foreach ($things as $values) {
+
+	print (var_dump($values)) . "\n";{
+		
+   
+	}
 }
+
+
+// //#3 echo values that are scalar
+foreach ($things as $values) {
+
+	if (is_scalar($values)) {
+		
+    echo $values . "\n";	
+	}
+
+}
+
+
+//#Create a loop that will echo out every value, including those nested in arrays. 
+//#4 echo all values
+//original doesn't echo values in second array. add if is_array + 2nd foreach
+foreach ($things as $values) {
+
+	if (is_array($values)) {
+		foreach($values as $inner_values)
+	
+    echo $inner_values . "\n";	
+	}
+
+	else {
+		echo $values. "\n";
+	}
+}
+
+

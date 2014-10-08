31dc3d705a193e528fe7065445f66c6bf51af7bf
DenaliE <denali.endicott@grok-interactive.com>
2014-10-08 10:57:28 -0500

    Changed to check if input numeric.

--- arithmetic.php
+++ arithmetic.php
@@ -1,32 +1,71 @@
 <?php
 
 function add($a, $b) {
-    echo $a + $b . PHP_EOL;
+    if (is_numeric($a) && is_numeric($b)){
+    	echo $a + $b . PHP_EOL;
+    }
+
+    else {
+    	echo "You need two numbers." . PHP_EOL;
+    }
 
 }
 
 add(5, 4);
 
 function subtract($a, $b) {
-    echo $a - $b . PHP_EOL;
+    if (is_numeric($a) && is_numeric($b)){
+    	echo $a - $b . PHP_EOL;
+    }
+
+    else {
+    	echo "You need two numbers." . PHP_EOL;
+    }
 }
 
  subtract(5, 4);
 
 function multiply($a, $b) {
-    echo $a * $b . PHP_EOL; 
+    if (is_numeric($a) && is_numeric($b)){
+    	echo $a * $b . PHP_EOL;
+    }
+
+    else {
+    	echo "You need two numbers." . PHP_EOL;
+    } 
 }
 
 multiply(5, 4);
 
 function divide($a, $b) {
-    echo $a / $b . PHP_EOL;
+    if (is_numeric($a) && is_numeric($b)){
+    	
+    	if ($b == 0) {
+    		echo "You cannot divide by zero." . PHP_EOL;
+    	}
+    	else {
+    		echo $a / $b . PHP_EOL;
+    	}
+    }
+
+
+    else {
+    	echo "You need two numbers." . PHP_EOL;
+    }
 }
 
 divide(5, 4);
 
 function modulus($a, $b) {
-    echo $a % $b . PHP_EOL;
-}
+    if (is_numeric($a) && is_numeric($b)){
+    	echo $a % $b . PHP_EOL;
+    }
+
+    else {
+    	echo "You need two numbers." . PHP_EOL;
+    } 
+} 
+
 
 modulus(5, 4);
+

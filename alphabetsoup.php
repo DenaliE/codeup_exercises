3a7b1065932af74b193cc755888fcdeb085bd2b0
DenaliE <denali.endicott@grok-interactive.com>
2014-10-20 17:02:14 -0500

    Removed extra code

--- alphabetsoup.php
+++ alphabetsoup.php
@@ -1,25 +1,5 @@
 <?php 
-//get input
-// $string = trim(fgets(STDIN));
 
-// //use space as delimiter for words
-// $words = explode(' ', $string);
-// print_r($words);
-
-// foreach ($words as $letters) {
-
-// //expects string
-// // //splits words into letters. Spaces included.
-// // $array = str_split($string);
-// // print_r($array);
-
-// //how do I sort the words individually?
-// sort($letters, SORT_NATURAL | SORT_FLAG_CASE);
-
-// var_dump($words);
-
-// //convert back to string?
-// // $sorted = implode(glue, pieces)
 echo "Please enter words to sort.  ";
 $string = trim(fgets(STDIN));
 
@@ -27,15 +7,11 @@ $string = trim(fgets(STDIN));
 $words = explode(' ', $string);
 
 foreach ($words as $word) {
-	// echo "$word\n";
-	// take each word
-	// split that word into an array of letters
-	// sort those letters
-	// ...
+	
 	$var = str_split($word);
-	//print_r($var);
+	
 	$sorted = sort($var);
-	// print_r(explode($sorted, $word));
+	
 	$var = implode('', $var);
 
 	// construct a string with this operator:  .=
